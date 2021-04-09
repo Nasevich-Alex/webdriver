df3c9fee862979bb457abe6493a95c7836bde1e6
Alex Nasevich <nasevichbsu@gmail.com>
2021-04-09 12:19:38 +0300

    add commit

--- src/test/java/bring_it_on/page/PastebinHP.java
+++ src/test/java/bring_it_on/page/PastebinHP.java
@@ -1,5 +1,6 @@
 package bring_it_on.page;
 
+
 import i_can_win.page.PastebinHomePage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -12,6 +13,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class PastebinHP {
     private static final String HOMEPAGE_URL = "https://pastebin.com/";
     private WebDriver driver;
+
 
     @FindBy(id = "postform-text")
     private WebElement newPasteTextArea;
@@ -34,6 +36,12 @@ public class PastebinHP {
     @FindBy(xpath = "//button[text()='Create New Paste']")
     private WebElement createNewPasteButton;
 
+    @FindBy(xpath = "/html/head/meta[5]")
+    private WebElement pageTitle;
+
+    @FindBy(xpath = ".//a[text()='Bash']")
+    private WebElement syntaxIsBash;
+
     public PastebinHP(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver, this);
@@ -46,5 +54,56 @@ public class PastebinHP {
         return this;
     }
 
+    public PastebinHP printCodeInNewPasteForm(String text) {
+        WebElement newPasteTextArea = waitForElementLocatedBy(driver, By.id("postform-text"));
+        newPasteTextArea.sendKeys(text);
+        return this;
+    }
 
+    public PastebinHP chooseSyntaxHighlightingForBash() {
+        syntaxHighlightingList.click();
+        bashLanguage.click();
+        return this;
+    }
+
+    public PastebinHP chooseExpirationForTenMinutes() {
+        pastExpirationDropDownList.click();
+        tenMinutesExpiration.click();
+        return this;
+    }
+
+    public PastebinHP pasteName(String name) {
+        WebElement pasteNameArea = waitForElementLocatedBy(driver, By.id("postform-name"));
+        pasteNameArea.sendKeys(name);
+        return this;
+    }
+
+    public PastebinHP clickNewPasteBtn() {
+        WebElement pasteNameArea = waitForElementLocatedBy(driver, By.xpath("//button[text()='Create New Paste']"));
+        createNewPasteButton.click();
+        return this;
+    }
+
+    public void createNewPasteWithConditions(String code, String name) {
+        openPage()
+                .printCodeInNewPasteForm(code)
+                .chooseSyntaxHighlightingForBash()
+                .chooseExpirationForTenMinutes()
+                .pasteName(name)
+                .clickNewPasteBtn();
+    }
+
+    public String getPageTitle() {
+        return driver.findElement(By.xpath("/html/head/meta[5]")).getText();
+    }
+
+    public boolean IsSyntaxABash() {
+        return (new WebDriverWait(driver, 10))
+                .until(ExpectedConditions.visibilityOf(syntaxIsBash)).isDisplayed();
+    }
+
+    private static WebElement waitForElementLocatedBy(WebDriver driver, By by) {
+        WebElement element = new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(by));
+        return element;
+    }
 }
--- src/test/java/bring_it_on/test/PastebinHPTest.java
+++ src/test/java/bring_it_on/test/PastebinHPTest.java
@@ -1,5 +1,53 @@
 package bring_it_on.test;
 
+
+import bring_it_on.page.PastebinHP;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 public class PastebinHPTest {
+    private WebDriver driver;
+    private PastebinHP resultPage;
+    private String code = "git config --global user.name  \"New Sheriff in Town\""
+            +"\n"+"git reset $(git commit-tree HEAD^{tree} -m \"Legacy code\")"
+            +"\n"+"git push origin master --force";
+    private String name = "how to gain dominance among developers";
+
+    @BeforeMethod(alwaysRun = true)
+    public void browserSetup() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        resultPage = new PastebinHP(driver);
+        resultPage.createNewPasteWithConditions(code, name);
+    }
+
+    @Test(description = "Check page title")
+    public void checkTitle() {
+        String resultTitle = resultPage.getPageTitle();
+        Assert.assertEquals(resultTitle, name);
+    }
+
+    @Test(description = "Syntax highlighted for bash")
+    public void checkHighlightingForBash() {
+        Assert.assertTrue(resultPage.IsSyntaxABash());
+    }
+
+    @Test(description = "Check code")
+    public void checkCode() {
+        String expectCode = code;
+        String resultCode = driver.findElement(By.xpath(".//textarea[@class='textarea']")).getText();
+        Assert.assertEquals(expectCode, resultCode);
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void browserDown() {
+        driver.quit();
+        driver = null;
+    }
 
 }
